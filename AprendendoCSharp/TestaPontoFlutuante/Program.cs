4d19c5e4b666c878f818f032d40a635c2b5ccf7d
Blarissa <larissabrasil009@gmail.com>
2023-01-11 15:01:10 -0300

    Adicionar arquivos de projeto.

---
+++ AprendendoCSharp/TestaPontoFlutuante/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestaPontoFlutuante
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = 1250.70;
+            Console.WriteLine("Meu salário é: " + salario);
+
+            double divisao =  5.0 / 2;
+            Console.WriteLine(divisao);
+        }
+    }
+}
