ea9d31f13ebae408ddc3fbfdae27aaa16084a33b
Blarissa <larissabrasil009@gmail.com>
2023-01-17 17:11:06 -0300

    Implementando Diretor

--- ByteBank/Funcionarios/Diretor.cs
+++ ByteBank/Funcionarios/Diretor.cs
@@ -6,17 +6,15 @@ using System.Threading.Tasks;
 
 namespace ByteBank.Funcionarios
 {
-    public class Funcionario
+    public class Diretor
     {
         public string Nome { get; set; }
-        public string CPF { get; set;}
+        public string CPF { get; set; }
         public double Salario { get; set; }
 
         public double GetBonificacao()
         {
-            if (_tipo == 1)
-                return Salario;
-            return Salario * 0.10;
+            return Salario;
         }
     }
 }
