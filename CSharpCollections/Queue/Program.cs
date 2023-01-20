f31aaa34ac7fa1d694a9fd9bdb56eedd24a395e2
Blarissa <larissabrasil009@gmail.com>
2023-01-20 20:42:38 -0300

    Usando Queue

---
+++ CSharpCollections/Queue/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    public class Program
+    {
+        static Queue<string> pedagio = new Queue<string>();
+
+        static void Main(string[] args)
+        {
+            Enfileirar("van");           
+            Enfileirar("kombi");            
+            Enfileirar("guincho");
+            Enfileirar("pickup");
+
+            Desenfileirar();
+            Desenfileirar();
+            Desenfileirar();
+
+
+            Console.ReadLine();
+        }
+
+        private static void Desenfileirar()
+        {
+            if (pedagio.Any())
+            {
+                if (pedagio.Peek() == "guincho")
+                    Console.WriteLine("Guincho está fazendo pagamento");
+
+                string veiculo = pedagio.Dequeue();
+                Console.Write($"Saiu na fila: {veiculo}\n");
+                ImprimirFila(); 
+            }
+        }
+
+        private static void Enfileirar(string veiculo)
+        {
+            Console.Write($"Entrou na fila: {veiculo}\n");
+            pedagio.Enqueue(veiculo);
+            ImprimirFila();
+        }
+        
+        private static void ImprimirFila()
+        {
+            Console.WriteLine("\nFILA: ");
+            foreach(var item in pedagio)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
