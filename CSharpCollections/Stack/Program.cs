b3d00da23f6c576edba295f42da44beb77c99d2a
Blarissa <larissabrasil009@gmail.com>
2023-01-20 16:57:34 -0300

    Usando Stack

---
+++ CSharpCollections/Stack/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var navegador = new Navegador();
+
+            navegador.NavegarPara("google.com");
+            navegador.NavegarPara("caelum.com.br");
+            navegador.NavegarPara("alura.com.br");
+            
+            navegador.Anterior();
+            navegador.Anterior();
+            navegador.Anterior();
+            navegador.Proximo();
+
+            Console.ReadLine();
+        }
+    }
+}
