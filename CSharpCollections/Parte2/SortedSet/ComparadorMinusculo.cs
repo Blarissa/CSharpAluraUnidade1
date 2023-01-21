bbdaf90df4d1739050327b96f3933843aaa1fc18
Blarissa <larissabrasil009@gmail.com>
2023-01-21 01:22:34 -0300

    Usando SortedSet

---
+++ CSharpCollections/Parte2/SortedSet/ComparadorMinusculo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedSet
+{
+    internal class ComparadorMinusculo : IComparer<string>
+    {
+        int IComparer<string>.Compare(string x, string y)
+        {
+            return string.Compare(x, y, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
---
+++ CSharpCollections/Parte2/SortedSet/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ISet<string> alunos = new SortedSet<string>(new ComparadorMinusculo()) 
+            { 
+                "Sherlock Holmes",
+                "Mycroft Holmes",
+                "Irene Adler",
+                "James Moriarty"
+            };
+
+            Console.WriteLine("Imprimindo SortedSet\n");
+            Imprimir(alunos);
+
+            alunos.Add("John Watson");
+            alunos.Add("Irene Adler");
+            alunos.Add("Irene ADLER");
+            alunos.Add("Mary Morstar");
+            alunos.Add("Martha Hudson");
+
+            Imprimir(alunos);
+
+
+            Console.ReadLine();
+        }
+        private static void Imprimir(ISet<string> alunos)
+        {
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
