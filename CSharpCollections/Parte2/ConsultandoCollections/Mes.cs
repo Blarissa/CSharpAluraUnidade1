8194028020cef27ee14d0e980c42f20e77422938
Blarissa <larissabrasil009@gmail.com>
2023-01-21 02:09:12 -0300

    Fazendo consulta com coleções

---
+++ CSharpCollections/Parte2/ConsultandoCollections/Mes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultandoCollections
+{
+    internal class Mes : IComparable
+    {
+        public Mes(string nome, int dias)
+        {
+            Nome = nome;
+            Dias = dias;
+        }
+
+        public string Nome { get; private set; }
+        public int Dias { get; private set; }
+
+        public int CompareTo(object obj)
+        {
+            Mes outro = obj as Mes;
+            return this.Nome.CompareTo(outro.Nome);
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Dias}";
+        }
+
+    }
+}
---
+++ CSharpCollections/Parte2/ConsultandoCollections/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultandoCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Mes> meses = new List<Mes>()
+            {
+                new Mes("Janeiro", 31),
+                new Mes("Fevereiro", 28),
+                new Mes("Janeiro", 31),
+                new Mes("Março", 30),
+                new Mes("Abril", 31),
+                new Mes("Junho", 30),
+                new Mes("Julho", 31),
+                new Mes("Agosto", 30),
+                new Mes("Setembro", 30),
+                new Mes("Outubro", 31),
+                new Mes("Novembro", 30),
+                new Mes("Dezembro", 31)
+            };
+
+            Imprimir(meses);
+
+            Console.ReadLine();
+
+        }
+
+        private static void Imprimir(List<Mes> meses)
+        {
+            meses.Sort();
+            meses.ForEach(mes => { 
+                if(mes.Dias == 31)
+                    Console.WriteLine(mes.Nome.ToUpper());
+            });
+        }
+    }
+}
