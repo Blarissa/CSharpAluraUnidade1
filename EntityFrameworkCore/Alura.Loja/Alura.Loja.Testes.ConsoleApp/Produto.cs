f4c6e1e22f58103c743d603ddbbf0a4a0c1ff949
Blarissa <larissabrasil009@gmail.com>
2023-01-22 03:34:37 -0300

    Corrigindo problemas e aplicando as migrações no banco

--- EntityFrameworkCore/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Produto.cs
+++ EntityFrameworkCore/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Produto.cs
@@ -5,14 +5,19 @@
         public int Id { get; internal set; }
         public string Nome { get; internal set; }
         public string Categoria { get; internal set; }
-        public double Preco { get; internal set; }
+        public double PrecoUnitario { get; internal set; }
+        public string Unidade { get; internal set; }
+
+
 
         public override string ToString()
         {
             return $"Produto N° {this.Id}\n" +
                 $"Nome: {this.Nome}\n" +
                 $"Categoria: {this.Categoria}\n" +
-                $"Preço: {this.Preco:C}\n\n";
+                $"Preço unitário: {this.PrecoUnitario:C}\n" +
+                $"Unidade: {this.Unidade}\n" +
+                $"\n";
         }
     }
 }
--- EntityFrameworkCore/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ EntityFrameworkCore/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -15,55 +15,7 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            using (var contexto = new LojaContext())
-            {
-                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
-                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
-                loggerFactory.AddProvider(SqlLoggerProvider.Create());
-
-                var produtos = contexto.Produtos.ToList();
-                
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-
-                /*
-                foreach (var p in produtos)
-                {
-                    Console.WriteLine(p);
-                }
-
-                var novoProduto = new Produto()
-                {
-                    Nome = "Desinfetante",
-                    Categoria = "Limpeza",
-                    Preco = 2.99
-                };
-
-                contexto.Produtos.Add(novoProduto);
-                */
-                var novoProduto = new Produto()
-                {
-                    Nome = "Sabão em pó",
-                    Categoria = "Limpeza",
-                    Preco = 5.99
-                };
-
-                contexto.Produtos.Add(novoProduto);
-
-                contexto.Produtos.Remove(novoProduto);
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                contexto.SaveChanges();
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                var entry = contexto.Entry(novoProduto);
-                Console.WriteLine("\n\n" + entry.Entity.ToString() + " - " + entry.State);
-
-            }
-
-
+            
 
             Console.ReadLine();
         }
