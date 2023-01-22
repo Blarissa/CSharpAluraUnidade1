e78589e3c00becf2f500bd69d7acdc301e1d99fa
Blarissa <larissabrasil009@gmail.com>
2023-01-22 02:29:49 -0300

    Logando o SQL no console

--- EntityFrameworkCore/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ EntityFrameworkCore/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,115 +15,64 @@ namespace Alura.Loja.Testes.ConsoleApp
         {
             using (var contexto = new LojaContext())
             {
-                var produtos = contexto.Produtos.ToList();
+                /*var produtos = contexto.Produtos.ToList();
 
-                produtos.ForEach( p => {
+                //produtos.ForEach(p =>
+                //{
+                //    Console.WriteLine(p);
+                //});
+
+                //Console.WriteLine("=================");
+                //foreach (var e in contexto.ChangeTracker.Entries())
+                //{
+                //    Console.WriteLine(e.State);
+                //}
+
+                //var p1 = produtos.Last();
+                //p1.Nome = "007 - O Espiao Que Me Amava";
+
+                ////contexto.SaveChanges();
+
+                //Console.WriteLine("=================");
+
+                //produtos = contexto.Produtos.ToList();
+
+                //foreach (var e in contexto.ChangeTracker.Entries())
+                //{
+                //    Console.WriteLine(e.State);
+                //}*/
+                //usando um provedor de serviços que vai fornecer um serviço específico que cria Loggers.
+                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
+                var produtos = contexto.Produtos.ToList();
+                foreach (var p in produtos)
+                {
                     Console.WriteLine(p);
-                });
+                }
 
                 Console.WriteLine("=================");
                 foreach (var e in contexto.ChangeTracker.Entries())
                 {
-                    Console.WriteLine(e.State);
+                    Console.WriteLine(e);
                 }
 
                 var p1 = produtos.Last();
                 p1.Nome = "007 - O Espiao Que Me Amava";
 
-                //contexto.SaveChanges();
-
                 Console.WriteLine("=================");
-
-                produtos = contexto.Produtos.ToList();
-
                 foreach (var e in contexto.ChangeTracker.Entries())
                 {
-                    Console.WriteLine(e.State);
+                    Console.WriteLine(e);
                 }
 
+                contexto.SaveChanges();
 
             }
 
             Console.ReadLine();
         }
-        /*
-        private static void AtualizarProduto()
-        {
-            // inclui um produto
-            GravarUsandoEntity();
-            RecuperarProdutos();
-
-            // atualiza o produto
-            using (var repo = new ProdutoDAOEntity())
-            {
-                Produto primeiro = repo.Produtos().First();
-
-                primeiro.Nome = "Cassino Royale - Editado"; 
-                repo.Atualizar(primeiro);
-            }
-            RecuperarProdutos();
-        }
-
-        private static void ExcluirProdutos()
-        {
-            using (var repo = new ProdutoDAOEntity()) { 
-                IList<Produto> produtos = repo.Produtos();
-
-                foreach(var item in produtos)
-                    repo.Remover(item);   
-            }
-        }
-
-        private static void RecuperarProdutos()
-        {
-            using (var repo = new ProdutoDAOEntity())
-            {
-                IList<Produto> produtos = repo.Produtos();
-
-                Console.WriteLine("Foram encontrados {0} produto(s)", produtos.Count);
-                foreach (var item in produtos)                
-                    Console.WriteLine(item.Nome);
-                
-                Console.WriteLine();
-            }
-        }
-
-        private static void GravarUsandoEntity()
-        {
-            Produto p1 = new Produto();
-            p1.Nome = "Harry Potter e a Ordem da Fênix";
-            p1.Categoria = "Livros";
-            p1.Preco = 19.89;
-
-            Produto p2 = new Produto();
-            p2.Nome = "Senhor dos Anéis 1";
-            p2.Categoria = "Livros";
-            p2.Preco = 19.89;
-
-            Produto p3 = new Produto();
-            p3.Nome = "O Monge e o Executivo";
-            p3.Categoria = "Livros";
-            p3.Preco = 19.89;
-
-            using (var repo = new ProdutoDAOEntity())
-            {
-                repo.Adicionar(p1);
-                repo.Adicionar(p2);
-                repo.Adicionar(p3);
-            }
-        }
-
-        private static void GravarUsandoAdoNet()
-        {
-            Produto p = new Produto();
-            p.Nome = "Harry Potter e a Ordem da Fênix";
-            p.Categoria = "Livros";
-            p.Preco = 19.89;
-
-            using (var repo = new ProdutoDAO())
-            {
-                repo.Adicionar(p);
-            }
-        }*/
     }
 }
