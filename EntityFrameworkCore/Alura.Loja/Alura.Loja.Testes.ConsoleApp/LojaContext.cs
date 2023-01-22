a133a13ce868b7c2a7a059b3f1f89d21f6735677
Blarissa <larissabrasil009@gmail.com>
2023-01-22 03:48:25 -0300

    Corrigindo a migração para refletir o relacionamento

--- EntityFrameworkCore/Alura.Loja/Alura.Loja.Testes.ConsoleApp/LojaContext.cs
+++ EntityFrameworkCore/Alura.Loja/Alura.Loja.Testes.ConsoleApp/LojaContext.cs
@@ -3,16 +3,11 @@ using System;
 
 namespace Alura.Loja.Testes.ConsoleApp
 {
-    public class LojaContext : DbContext
+    internal class LojaContext : DbContext
     {
         public DbSet<Produto> Produtos { get; set; }
-
-        public LojaContext()
-        { }
-
-        public LojaContext(DbContextOptions<LojaContext> options) : base(options)
-        { }
-
+        public DbSet<Compra> Compras { get; set; }
+        
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if(!optionsBuilder.IsConfigured)
--- EntityFrameworkCore/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ EntityFrameworkCore/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -15,7 +16,17 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            
+            // compra de 6 pães franceses
+            var paoFrances = new Produto();
+            paoFrances.Nome = "Pão Francês";
+            paoFrances.PrecoUnitario = 0.40;
+            paoFrances.Unidade = "Unidade";
+            paoFrances.Categoria = "Padaria";
+
+            var compra = new Compra();
+            compra.Quantidade = 6;
+            compra.Produto = paoFrances;
+            compra.Preco = paoFrances.PrecoUnitario * compra.Quantidade;
 
             Console.ReadLine();
         }
